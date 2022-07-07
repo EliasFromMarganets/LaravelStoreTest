782dce719dfad39eef9b27cb196f65744bc77f6c
Illia Samoilenko@ <samoilenkoim1994@gmail.com>
2022-07-07 18:05:09 +0300

    Changes after making product page and links in home page to it

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function showItem($category, $id) {
+        $product_item = Product::where('id', $id)->first();
+
+        return view('product.show_item',[
+            'item' => $product_item,
+        ]);
+    }
+}
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -153,14 +153,22 @@
                                     $image = 'no_image.png';
                                 }
                             @endphp
-                        <div class="product">
-                            <div class="product_image"><img src="images/{{$image}}" alt="{{$product->title}}"></div>
-                            <div class="product_extra product_new"><a href="categories.html">New</a></div>
-                            <div class="product_content">
-                                <div class="product_title"><a href="product.html">{{$product->title}}</a></div>
-                                <div class="product_price">{{$product->price}}$</div>
+                            <div class="product">
+                                <div class="product_image"><img src="images/{{$image}}" alt="{{$product->title}}"></div>
+                                <div class="product_extra product_new"><a href="categories.html">New</a></div>
+                                <div class="product_content">
+                                    <div class="product_title">
+                                        <a href="{{route('showItem',['category', $product->id])}}">{{$product->title}}
+                                        </a>
+                                    </div>
+                                    @if($product->new_price > 0)
+                                        <div style="text-decoration: line-through">${{$product->price}}</div>
+                                        <div class="product_price">${{$product->new_price}}</div>
+                                    @else
+                                        <div class="product_price">${{$product->price}}</div>
+                                    @endif
+                                </div>
                             </div>
-                        </div>
                         @endforeach
                     </div>
                 </div>
@@ -234,31 +242,5 @@
             </div>
         </div>
     </div>
-
-    <!-- Newsletter -->
-
-    <div class="newsletter">
-        <div class="container">
-            <div class="row">
-                <div class="col">
-                    <div class="newsletter_border"></div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-8 offset-lg-2">
-                    <div class="newsletter_content text-center">
-                        <div class="newsletter_title">Subscribe to our newsletter</div>
-                        <div class="newsletter_text"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-                                a ultricies metus. Sed nec molestie eros</p></div>
-                        <div class="newsletter_form_container">
-                            <form action="#" id="newsletter_form" class="newsletter_form">
-                                <input type="email" class="newsletter_input" required="required">
-                                <button class="newsletter_button trans_200"><span>Subscribe</span></button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    @include('layouts.newsletter')
 @endsection
